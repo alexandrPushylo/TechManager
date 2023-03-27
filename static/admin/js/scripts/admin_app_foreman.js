50c203d3f91b00fbd776eb34602ca3c6464d074d
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2023-03-27 13:47:06 +0300

    Update admin_app_foreman.js

--- static/admin/js/scripts/admin_app_foreman.js
+++ static/admin/js/scripts/admin_app_foreman.js
@@ -3,6 +3,35 @@ cw_date.change(function () {
     location.href = '/applications/'+this.id+'/'+this.value;
 })
 
+$('#btn_show_tech').click(function () {
+    sent('technics')
+    return false
+})
+
+$('#btn_show_all').click(function () {
+    sent('all')
+    return false
+})
+
+$('#btn_show_mater').click(function () {
+    sent('materials')
+})
+
+function sent(i) {
+        $.ajax({
+        type: 'POST',
+        mode: 'same-origin',
+        url: pathname,
+        data:{
+            csrfmiddlewaretoken: csrf,
+                filter: i
+            },
+        success: function() {
+            window.location.reload();
+            }
+        })
+}
+
 $('#container').masonry({
 // указываем элемент-контейнер в котором расположены блоки для динамической верстки
 	  itemSelector: '.item',
