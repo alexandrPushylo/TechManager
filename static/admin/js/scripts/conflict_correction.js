1431aa9f6041047e7c2bdfbd4c6be4d3385e623e
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2023-04-03 13:54:24 +0300

    rebu conflict_cor

--- static/admin/js/scripts/conflict_correction.js
+++ static/admin/js/scripts/conflict_correction.js
@@ -1,54 +1,30 @@
-console.log('fdf')
 const count_vehicles = $('.input_tech').length;
 
 for(let i=1;i<=count_vehicles;i++){
-    let input_tech = '#input_tech_'+i;
-    let input_driver = '#input_driver_'+i;
-    let io_veh = '#io_vehicle_'+i;
-    let io_drv = '#io_driver_'+i;
-    let btn_reset = '#btn_reset_'+i;
+    const btn_reset = '#btn_reset_'+i;
+    const io_vehicle = '#io_vehicle_'+i;
+    const io_driver = '#io_driver_'+i;
+    const select_tech_name = '#select_tech_name_'+i;
+    const select_tech_drv = '.select_tech_drv_'+i;
 
-    let curr_vehicle = $(input_tech);
-    let curr_driver = $(input_driver);
-    let curr_io_veh = $(io_veh);
-    let curr_io_drv = $(io_drv);
-    let curr_btn_rst = $(btn_reset);
+    const io_vehicle_id_val = $(io_vehicle).val();
+    const io_driver_id_val = $(io_driver).val();
 
-    const v_tech = $(io_veh).val();
-    const v_drv = $(io_drv).val();
+    $('#select_tech_name_'+i+'> option[value="'+ io_vehicle_id_val +'"]').prop('selected',true);
+    $('#select_tech_drv_'+io_vehicle_id_val+select_tech_drv).prop('disabled', false).prop('hidden',false);
+    $('#select_tech_drv_'+io_vehicle_id_val+select_tech_drv+' >option[value="'+ io_driver_id_val +'"]').prop('selected',true);
 
-
-
-    $(input_tech+'> option[value="'+ v_tech +'"]').prop('selected',true);
-    $(input_driver +'> option[veh="'+ v_tech+'"]').attr('hidden',false);
-    if(v_drv === ''){
-        $(input_driver +" > option:checked").val('').text('---');
-    }else {
-        $(input_driver+'> option[value="'+ v_drv +'"]').prop('selected',true);
-    }
-
-    curr_btn_rst.click(function () {
-        $(input_driver +'> option[value="'+ curr_io_drv.val() +'"]').first().prop('selected',true);
-        $(input_tech +'> option[value="'+ curr_io_veh.val()+'"]').first().prop('selected',true);
+    $(btn_reset).click(function () {
+        $(select_tech_drv).prop('disabled', true).prop('hidden',true);
+        $('#select_tech_drv_'+io_vehicle_id_val+select_tech_drv).prop('disabled', false).prop('hidden',false);
+        $('#select_tech_name_'+i+'> option[value="'+ io_vehicle_id_val +'"]').prop('selected',true);
+        $('#select_tech_drv_'+io_vehicle_id_val+select_tech_drv+' >option[value="'+ io_driver_id_val +'"]').prop('selected',true);
         return false;
     });
 
-    curr_vehicle.change(function () {
-        $(input_driver +'> option').attr('hidden',true);
-        $(input_driver +"> option[selected]").attr('hidden',true);
-        const checked_tech = this.value;
-        $(input_driver +'> option[veh="'+ checked_tech+'"]').attr('hidden',false);
-
-        if ($(input_driver +" option[hidden!='hidden']").length < 2){
-        $(input_driver +" option[hidden!='hidden']:last").prop('selected', true);
-        }
-        else {
-            $(input_driver +" option[hidden!='hidden']:first").prop('selected', true);
-        // $(input_driver +" > option:checked").val('').text('---');
-        }
-
-
-
+    $(select_tech_name).change(function () {
+        $(select_tech_drv).prop('disabled', true).prop('hidden',true);
+        $('#select_tech_drv_'+this.value+select_tech_drv).prop('disabled', false).prop('hidden',false);
     });
 }
 
