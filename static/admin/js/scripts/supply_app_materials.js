8307b496c4cb2221562b58421e9be7dc0f413da5
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2023-12-28 15:10:17 +0300

    ref supply_app_mater

--- static/admin/js/scripts/supply_app_materials.js
+++ static/admin/js/scripts/supply_app_materials.js
@@ -7,13 +7,6 @@ $('.app_description').each(function () {
     this.style.height = ""+(this.scrollHeight)+"px";
 });
 
-// $('.app_description').on('input', function(){
-// 	// this.style.height = '1px';
-//     this.style.height = this.scrollHeight;
-// 	// this.style.height = (this.scrollHeight + 6) + 'px';
-// });
-
-
 const csrf = $('input[name="csrfmiddlewaretoken"]').val();
 const pathname = window.location.pathname;
 
@@ -22,11 +15,14 @@ $('.app_description').on('input', function () {
     $('#div'+desc_id).removeClass('border','border-success', 'border-2');
     $('#btn_save').val('Сохранить изменения');
     $('#btn_save').removeClass('btn-danger').addClass('btn-warning');
-})
+    $('#btn_ch_'+desc_id).val('Сохранить')
+    $('#btn_ch_'+desc_id).removeClass('btn-outline-success').addClass('btn-warning');
+    $('#labl_'+desc_id).text('Не сохранено');
+});
 
 $('.btn_print').click(function () {
     window.open(pathname.replace('materials','print'))
 
-})
+});
 
 
